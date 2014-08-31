9bd1b149dc22959b3a3252acec15be7194d21afb
zhangweilin <zwllxs@qq.com>
2014-08-31 21:56:08 +0800

    init24

--- testGit4/src/testGit4/Test.java
+++ testGit4/src/testGit4/Test.java
@@ -5,6 +5,12 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("teswcxsfdst");
+		System.out.println("teswcxsfdst");
+		System.out.println("teswcxsfdst");
+		System.out.println("teswcxsfdst");
+		System.out.println("teswcxsfdst");
+		System.out.println("teswcxsfdst");
+		System.out.println("teswcxsfdst");
 	} 
    
 }
